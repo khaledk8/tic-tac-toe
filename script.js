50d7ef6383a69dd1ee34348e99e47444ce3800b4
khaledk8 <khalidkari1@gmail.com>
2024-05-09 09:13:20 +0300

    Implemented win condition

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 let Gameboard = {}
 
 const players = {
-    playerX: 'X',
-    playerO: 'Y'
+    playerX: {playerName: 'x', value: 'x'},
+    playerO: {playerName: 'o', value: 'o'}
 }
 
 let activePlayer
@@ -42,5 +42,52 @@ function restart () {
 } */
 
 function switchPlayer () {
-    activePlayer == players.playerX ? activePlayer = players.playerO : activePlayer = playerX
+    activePlayer == players.playerX ? activePlayer = players.playerO : activePlayer = players.playerX
+}
+
+function printOnBoard (x, y, player = activePlayer) {
+    if (Gameboard.gameboard[x][y] == 0) {
+        Gameboard.gameboard[x][y] = player.value
+        switchPlayer()
+    }
+    winCondition()
+
+}
+
+function winCondition () {
+    if (Gameboard.gameboard[0][0] == Gameboard.gameboard[0][1] && Gameboard.gameboard[0][0] == Gameboard.gameboard[0][2] && Gameboard.gameboard[0][0] != 0) {
+        winner(0,0)
+    }
+
+    if (Gameboard.gameboard[1][0] == Gameboard.gameboard[1][1] && Gameboard.gameboard[1][0] == Gameboard.gameboard[1][2] && Gameboard.gameboard[1][0] != 0) {
+        winner(1,0)
+    }
+
+    if (Gameboard.gameboard[2][0] == Gameboard.gameboard[2][1] && Gameboard.gameboard[2][0] == Gameboard.gameboard[2][2] && Gameboard.gameboard[2][0] != 0) {
+        winner(2,0)
+    }
+
+    if (Gameboard.gameboard[0][0] == Gameboard.gameboard[1][1] && Gameboard.gameboard[0][0] == Gameboard.gameboard[2][2] && Gameboard.gameboard[0][0] != 0) {
+        winner(0,0)
+    }
+
+    if (Gameboard.gameboard[0][0] == Gameboard.gameboard[1][0] && Gameboard.gameboard[0][0] == Gameboard.gameboard[2][0] && Gameboard.gameboard[0][0] != 0) {
+        winner(0,0)
+    }
+
+    if (Gameboard.gameboard[0][1] == Gameboard.gameboard[1][1] && Gameboard.gameboard[0][1] == Gameboard.gameboard[2][1] && Gameboard.gameboard[0][1] != 0) {
+        winner(0,1)
+    }
+
+    if (Gameboard.gameboard[0][2] == Gameboard.gameboard[1][2] && Gameboard.gameboard[0][2] == Gameboard.gameboard[2][2] && Gameboard.gameboard[0][2] != 0) {
+        winner(0,2)
+    }
+    
+    if (Gameboard.gameboard[0][2] == Gameboard.gameboard[1][1] && Gameboard.gameboard[0][2] == Gameboard.gameboard[2][2] && Gameboard.gameboard[0][2] != 0) {
+        winner(0,2)
+    }
+}
+
+function winner (x, y) {
+    console.log(`Player ${Gameboard.gameboard[x][y]} wins`)
 }
